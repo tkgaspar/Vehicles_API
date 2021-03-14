6e770a00c614c716fdb6f63122d08ae60c1e643b
tkgaspar <tkgaspar@yahoo.de>
2021-03-14 11:12:18 +0200

    Initial commit

---
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -0,0 +1,9 @@
+package com.udacity.pricing.domain.price;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface PriceRepository extends CrudRepository<Price,Long> {
+
+}
---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (ID, currency, price, vehicle_id) VALUES (1, 'USD', 14500, 1);
+INSERT INTO price (ID, currency, price, vehicle_id) VALUES (2, 'USD', 13200, 2);
+INSERT INTO price (ID, currency, price, vehicle_id) VALUES (3, 'USD', 79800, 3);
+INSERT INTO price (ID, currency, price, vehicle_id) VALUES (4, 'EUR', 16555, 4);
+INSERT INTO price (ID, currency, price, vehicle_id) VALUES (5, 'EUR', 14444, 14);
